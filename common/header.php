e4e1ec2e4aa3c7ef9e5e22711a203f3edd5d5b4e
Katie Knox <kknox1@swarthmore.edu>
2019-03-01 13:37:36 -0500

    change overlay location

--- common/header.php
+++ common/header.php
@@ -35,14 +35,18 @@
     <?php echo head_js(); ?>
 </head>
  <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
- <div id="overlay">
-   <div id="overlaySolid">
+ <!-- <div id="overlay"> -->
+   <!-- <div id="overlaySolid"> -->
     <a href="#content" id="skipnav"><?php echo __('Skip to main content'); ?></a>
     <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
         <header role="banner">
             <?php fire_plugin_hook('public_header', array('view'=>$this)); ?>
             <div id="site-title"><?php echo link_to_home_page(theme_logo()); ?></div>
-
+            <!-- ADD LOGO-->
+            <div id="logo" tabindex="">
+                <a href="index.html"><img src="themes/ecr-theme/img/cr_logo.svg" alt="Swarthmore's Oral Histories"></a>
+            <!--  <div id="site-title"><a href="index.html">Oral Histories</a> -->
+            </div>
             <div id="search-container" role="search">
                 <?php if (get_theme_option('use_advanced_search') === null || get_theme_option('use_advanced_search')): ?>
                 <?php echo search_form(array('show_advanced' => true)); ?>
@@ -62,8 +66,8 @@
              ?>
            </div>
         <?php echo theme_header_image(); ?>
-      </div>
-    </div>
+      <!-- </div> -->
+    <!--</div>-->
   <div id="content" role="main" tabindex="-1">
 
 
