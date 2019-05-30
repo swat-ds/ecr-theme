2205b62189040fce5f41504ca92e62dc5330ffd1
Katie Knox <kknox1@swarthmore.edu>
2019-05-30 15:53:33 -0400

    documentation in scss

--- common/header.php
+++ common/header.php
@@ -35,13 +35,11 @@
     <?php echo head_js(); ?>
 </head>
  <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
- <!-- removed reference to overlay--background is formatted using linear gradient, see _background.scss-->
     <a href="#content" id="skipnav"><?php echo __('Skip to main content'); ?></a>
     <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
         <header role="banner">
             <?php fire_plugin_hook('public_header', array('view'=>$this)); ?>
             <div id="site-title"><?php echo link_to_home_page(theme_logo()); ?></div>
-            <!--removed redundant logo -->
             <div id="search-container" role="search">
                 <?php if (get_theme_option('use_advanced_search') === null || get_theme_option('use_advanced_search')): ?>
                 <?php echo search_form(array('show_advanced' => true)); ?>
